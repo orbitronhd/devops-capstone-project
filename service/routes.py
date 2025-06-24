fe3ef8bd7f482fb9e9491073bfdf5e2a259447a0
orbitronhd <orbitron.hd.official@gmail.com>
2025-06-24 11:55:35 -0400

    routes.py fix

--- service/routes.py
+++ service/routes.py
@@ -38,6 +38,7 @@ def index():
 ######################################################################
 # CREATE A NEW ACCOUNT
 ######################################################################
+
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
     """
@@ -60,6 +61,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -77,6 +79,7 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -94,6 +97,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
     """
@@ -104,7 +108,7 @@ def update_account(account_id):
     """
     app.logger.info("Request to update account with id: %s", account_id)
     check_content_type("application/json")
-    
+ 
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
@@ -128,7 +132,7 @@ def delete_account(account_id):
     This endpoint will delete an Account based on the account_id that is passed in.
     """
     app.logger.info("Request to delete account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
